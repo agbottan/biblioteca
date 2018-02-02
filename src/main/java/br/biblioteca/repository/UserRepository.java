c5ca4678eddb0090e494fa4729a9223cebf9eb3f
André G. Bottan <agbottan@gmail.com>
2018-02-02 18:46:45 -0200

    User Impl

--- src/main/java/br/biblioteca/repository/UserRepository.java
+++ src/main/java/br/biblioteca/repository/UserRepository.java
@@ -1,56 +1,11 @@
 package br.biblioteca.repository;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.biblioteca.beans.Role;
 import br.biblioteca.beans.User;
 
-@Repository
-public class UserRepository {
-	
-	List<User> users = new ArrayList<>();
+public interface UserRepository  extends JpaRepository <User, Long> {
 
-	UserRepository() {
-		
-		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		
-		/* !!!
-        User basic = new User("teste", passwordEncoder.encode("123456"));
-        basic.getRoles().add(new Role("ROLE_BASIC"));
-        users.add(basic);
-        
-        User admin = new User("admin", passwordEncoder.encode("123456"));
-        admin.getRoles().add(new Role("ROLE_BASIC"));
-        admin.getRoles().add(new Role("ROLE_ADMIN"));
-        users.add(admin);
-        */
-	}
-
-	public User findByUsername(String username) {
-
-		User user = null;
-
-		for (User u : users) {
-
-			if (u.getUsername().equals(username)) {
-				user = u;
-			}
-
-		}
-
-		return user;
-	}
-
-	public void save(User user) {
-		users.add(user);
-	}
-
-	public List<User> findAll(){
-		return users;
-	}
+	public User findByUsername(String username);
 }
